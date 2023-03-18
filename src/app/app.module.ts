0861f3a6f0e3da60da7c7a67b2620c08e51de384
Beka Sturua <beqasturua3@gmail.com>
2023-03-18 22:02:11 +0400

    ng g c game-control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GameControlComponent,
   ],
   imports: [
     BrowserModule,
