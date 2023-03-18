e5b0639d274d991db0b8392e6c607ec0124e4667
Beka Sturua <beqasturua3@gmail.com>
2023-03-18 22:02:57 +0400

    ng g c odd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControlComponent,
+    OddComponent,
   ],
   imports: [
     BrowserModule,
